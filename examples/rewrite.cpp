3a2a1da12044d2610da0eaa12788812da61c9779
jiunhaochen <duncan11514@gmail.com>
2024-03-23 16:09:04 +0800

    smt v1

--- examples/rewrite.cpp
+++ examples/rewrite.cpp
@@ -4,13 +4,15 @@
 
 int main(int argc, char *argv[]) {
     sword::RWManager man;
-    man.createWord("a", 3, 3);
-    printf("\n");
-    man.createWord("b", 3, 3);
-    printf("\n");
-    // man.createWord("c", 3, 3);
 
-    man.parsePoly("np3_01.poly");
+    if (argc != 2) {
+        printf("usage: ./%s <filepath>\n", argv[0]);
+        return 0;
+    }
+
+    man.parsePoly(argv[1]);
+    printf("(check-sat)\n");
+    printf("(get-model)\n");
     
     return 0;
 }
--- src/Rewrite.cpp
+++ src/Rewrite.cpp
@@ -18,23 +18,23 @@ namespace sword {
 void RWManager::createWord(std::string name, int inbit, int outbit) {
     std::vector<std::string> coeff_bit(inbit);
     for (int i = 0; i < inbit; ++i) {
-        coeff_bit[i] = name + "_c" + "_" + std::to_string(i) + "";
+        coeff_bit[i] = name + "_coeff" + "_" + std::to_string(i) + "";
     }
     PI(coeff_bit, outbit);
-    std::string coeff = name + "_c";
+    std::string coeff = name + "_coeff";
     // std::cout << "(declare-const " << coeff << " (_ BitVec " << outbit << "))" << std::endl;
     std::cout << "(declare-const " << coeff << " Int)" << std::endl;
 
     std::vector<std::string> neg(inbit);
     for (int i = 0; i < inbit; ++i) {
-        neg[i] = name + "_n" + "_" + std::to_string(i) + "";
+        neg[i] = name + "_neg" + "_" + std::to_string(i) + "";
     }
     PB(neg);
 
     std::vector<std::vector<std::string> > bit_coeff_bit(inbit, std::vector<std::string>(outbit));
     for (int i = 0; i < inbit; ++i) { // coeff
         for (int j = 0; j < outbit; ++j) { // bit of coeff
-            bit_coeff_bit[i][j] = name + "_b" + "^c_" + std::to_string(i) + "" + "_" + std::to_string(j) + "";
+            bit_coeff_bit[i][j] = name + "_bit_" + std::to_string(i) + "_coeff_" + std::to_string(j) + "";
         }
     }
     for (auto c : bit_coeff_bit) {
@@ -42,34 +42,39 @@ void RWManager::createWord(std::string name, int inbit, int outbit) {
     }
     std::vector<std::string> bit_coeff(outbit);
     for (int i = 0; i < outbit; ++i) {
-        bit_coeff[i] = name + "_b" + "_" + std::to_string(i) + ""; 
+        bit_coeff[i] = name + "_bit" + "_" + std::to_string(i) + ""; 
     }
     PB(bit_coeff);
 
     // constraint
     for (int i = 0; i < inbit; ++i) {
-        std::string poly_coeff = bit_coeff_bit[i][0];
-        for (int j = 1; j < outbit; ++j) {
+        std::string poly_coeff = (outbit > 1) ? "(+ " : "";
+        for (int j = 0; j < outbit; ++j) {
             int power = 1 << j;
-            std::string poly_new_term = "(* " + std::to_string(power) + " " + bit_coeff_bit[i][j] + ")";
-            poly_coeff = "(+ " + poly_coeff + " " + poly_new_term + ")";
+            std::string poly_new_term;
+            if (power != 1)
+                poly_new_term = std::string("(* ") + std::to_string(power) + " " + bit_coeff_bit[i][j] + ")";
+            else
+                poly_new_term = bit_coeff_bit[i][j];
+            poly_coeff = poly_coeff + poly_new_term + " ";
         }
-        std::string constraint = std::string("(- (* ") + "(- 1 " + neg[i] + ")" + " " + poly_coeff + ") (* " + neg[i] + " " + poly_coeff + "))";
+        poly_coeff += (outbit > 1) ? ")" : "";
+
+        std::string beta = name + "_beta_" + std::to_string(i);
+        printf("(declare-const %s Int)\n", beta.c_str());
+        printf("(assert (= %s %s))\n", beta.c_str(), poly_coeff.c_str());
+        std::string constraint = std::string("(- (* ") + "(- 1 " + neg[i] + ")" + " " + beta + ") (* " + neg[i] + " " + beta + "))";
         constraint = std::string("(= ") + coeff_bit[i] + " " + constraint + ")";
         constraint = std::string("(assert ") + constraint + ")";
         std::cout << constraint << std::endl;
     }
-    std::string constraint = std::string("(* ") + bit_coeff_bit[0][0] + " " + neg[0] + ")";
+
+    std::string constraint = "(+ ";
     for (int i = 0; i < inbit; ++i) {
-        for (int j = 0; j < outbit; ++j) {
-            if (i == 0 && j == 0) continue;
-            int power = 1 << j;
-            std::string poly_new_term = "(* " + bit_coeff_bit[i][j] + " " + neg[i] + ")";
-            if (power != 1)
-                poly_new_term = "(* " + std::to_string(power) + " " + poly_new_term + ")";
-            constraint = "(+ " + constraint + " " + poly_new_term + ")";
-        }
+        std::string beta = name + "_beta_" + std::to_string(i);
+        constraint = constraint + "(* " + neg[i] + " " + beta + ") ";
     }
+
     for (int j = 0; j < outbit; ++j) {
         int power = 1 << j;
         std::string poly_new_term;
@@ -77,8 +82,9 @@ void RWManager::createWord(std::string name, int inbit, int outbit) {
             poly_new_term = std::string("(* ") + std::to_string(power) + " " + bit_coeff[j] + ")";
         else
             poly_new_term = bit_coeff[j];
-        constraint = "(+ " + constraint + " " + poly_new_term + ")";
+        constraint = constraint + " " + poly_new_term;
     }
+    constraint += ")";
     constraint = std::string("(= ") + coeff + " " + constraint + ")";
     constraint = std::string("(assert ") + constraint + ")";
     std::cout << constraint << std::endl;
@@ -94,11 +100,11 @@ void RWManager::createWord(std::string name, int inbit, int outbit) {
 
     std::string poly_str;
     for (int i = 0; i < inbit; ++i) {
-        poly_str = poly_str + "(1 " + std::to_string(outbit) + " " + name + "_c_" + std::to_string(i) + " 1 " + std::to_string(inbit) + " " + name + "_" + std::to_string(i) + " 1)";
+        poly_str = poly_str + "(1 " + std::to_string(1) + " " + name + "_coeff_" + std::to_string(i) + " 1 " + std::to_string(1) + " " + name + "_" + std::to_string(i) + " 1)";
         if (i < inbit)
             poly_str += " ";
     }
-    poly_str = poly_str + "(1 " + std::to_string(outbit) + " " + name + "_c" + " 1)"; 
+    poly_str = poly_str + "(1 " + std::to_string(1) + " " + name + "_coeff" + " 1)"; 
     // std::cout << "|" << poly_str << "|" << std::endl;
     Polynomial poly = poly_man.parse(poly_str);
     poly_pool[name] = poly_vec.size();
@@ -106,14 +112,121 @@ void RWManager::createWord(std::string name, int inbit, int outbit) {
 }
 
 void RWManager::parsePoly(std::string filepath) {
-    int inbit = 3;
-    int outbit = 3;
-    std::vector<std::string> vars = {"a_0", "a_1", "a_2", "b_0", "b_1", "b_2"};
     std::ifstream f(filepath);
     std::string line;
+
+    int outbit = 8;
+
+    // Read number of variable
+    std::getline(f, line);
+    int num_variable = std::stoi(line);
+    // Read variable
+    std::vector<std::pair<std::string, int> > v;
+    for (int i = 0; i < num_variable; ++i) {
+        std::getline(f, line);
+        std::string name = line.substr(0, line.find(" "));
+        int bits = std::stoi(line.substr(line.find(" ") + 1));
+        v.push_back({name, bits});
+    }
+
+    // Generate all vars
+    std::unordered_map<std::string, int> var_bit;
+    std::vector<std::string> vars;
+    for (auto e : v) {
+
+        this->createWord(std::get<0>(e), std::get<1>(e), outbit);
+        printf("\n");
+
+        var_bit[std::get<0>(e)] = std::get<1>(e);
+        for (int i = 0; i < std::get<1>(e); ++i) {
+            std::string var = std::get<0>(e) + "_" + std::to_string(i);
+            vars.push_back(var);
+        }
+    }
+
+    // Read support information
+    for (int i = 0; i < num_variable; ++i) {
+        std::string name = std::get<0>(v[i]);
+        int bits = std::get<1>(v[i]);
+        std::vector<std::set<std::string> > order;
+        // prepare set
+        std::set<std::string> bucket;
+        for (int j = 0; j < bits; ++j) {
+            bucket.insert(name + "_" + std::to_string(j));
+        }
+        order.push_back(bucket);
+        int cover_bits = 0;
+
+        for (int j = 0; j < bits; ++j) {
+            std::getline(f, line);
+            std::stringstream ss(line);
+            int num;
+            std::string current, include;
+            ss >> current >> num;
+            cover_bits += num > 0;
+
+            // Find bucket according to current
+            int current_index;
+            for (current_index = 0; current_index < order.size(); ++current_index) {
+                if (order[current_index].count(current)) {
+                    break;
+                }
+            }
+            auto &cbucket = order[current_index];
+
+            // printf("%s[%d]: ", current.c_str(), num);
+
+            std::set<std::string> nbucket;
+            for (int k = 0; k < num; ++k) {
+                ss >> include;
+                // printf("%s ", include.c_str());
+
+                if (cbucket.count(include)) {
+                    cbucket.erase(include);
+                    nbucket.insert(include);
+                }
+            }
+
+            if (!nbucket.empty()) {
+                if (current_index != order.size() - 1) {
+                    order[current_index+1].insert(nbucket.begin(), nbucket.end());
+                } else {
+                    order.push_back(nbucket);
+                }
+            }
+
+            // printf("\n");
+        }
+        // for (int i = 0; i < order.size(); ++i) {
+        //     for (auto b : order[i]) {
+        //         printf("%s ", b.c_str());
+        //     }
+        //     printf("\n");
+        // }
+
+        int uncertain_bits = bits - cover_bits;
+
+        int current_bit_start = 0;
+        for (int i = 0; i < order.size(); ++i) {
+            std::set<std::string> bucket = order[i];
+            int current_bit_end = current_bit_start + bucket.size() - 1;
+            // printf("%d %d\n", current_bit_start, current_bit_end);
+            for (auto b : bucket) {
+                b = b.substr(b.find("_")+1);
+
+                for (int forbidden = 0; forbidden < bits; ++forbidden) {
+                    if (forbidden >= current_bit_start && forbidden <= current_bit_end + uncertain_bits) continue;
+                    printf("(assert (not %s_bit_%s_coeff_%d))\n", name.c_str(), b.c_str(), forbidden);
+                }
+            }
+
+            current_bit_start = current_bit_end + 1;
+        }
+    }
+
+    // Get Terms
     std::vector<std::string> terms;
     std::set<std::set<std::string> > term_set;
-
     while (std::getline(f, line)) {
         terms.emplace_back(line);
     }
@@ -125,6 +238,10 @@ void RWManager::parsePoly(std::string filepath) {
         int nb = std::count(b.begin(), b.end(), '*');
         return na > nb;
     });
+
+    for (auto e : v) {
+        term_set.insert({std::get<0>(e)});
+    }
 
     for (auto term : terms) {
         std::set<std::string> var_set;
@@ -142,14 +259,21 @@ void RWManager::parsePoly(std::string filepath) {
     int s_n = 0;
     for (auto term : term_set) {
         printf("(declare-const s_%d Int)\n", s_n);
-        std::string s = "(1 " + std::to_string(outbit) + " s_" + std::to_string(s_n++) + " 1)";
+        printf("(assert (and (>= s_%d 0) (< s_%d %d)))\n", s_n, s_n, (1 << outbit));
+        printf("; s_%d * ");
+        std::string s = "(1 " + std::to_string(1) + " s_" + std::to_string(s_n++) + " 1)";
         Polynomial poly_term = poly_man.parse(s);
+        // PRINT_V(term);
         for (auto t : term) {
+            printf("%s * ", t.c_str());
             Polynomial p = getWord(t);
             poly_term = poly_man.poly_mul(poly_term, p);
         }
+        printf("\n", poly_term.str().c_str());
         poly_assume = poly_man.poly_add(poly_assume, poly_term);
     }
+    // print_poly(poly_assume);
+    Polynomial poly_remain = poly_assume;
 
     // coeff observation
     for (auto term : terms) {
@@ -164,8 +288,8 @@ void RWManager::parsePoly(std::string filepath) {
             if (pos == std::string::npos) break;
             term = term.substr(pos+1);
             int var_pos = term.find("*");
-            poly_str = poly_str + std::to_string(inbit) + " ";
             std::string var = (var_pos == std::string::npos) ? term : term.substr(0, var_pos);
+            poly_str = poly_str + std::to_string(1) + " ";
             poly_str = poly_str + var + " 1 ";
         } while (1);
         poly_str = poly_str + ")";
@@ -200,15 +324,20 @@ void RWManager::parsePoly(std::string filepath) {
                 printf(")");
             } 
             printf(" %d)))\n", (1 << outbit));
+
+            poly_remain = poly_man.poly_sub(poly_remain, coeff);
             continue;
         }
+        // print_poly(poly_assume);
         Polynomial p = poly_man.parse(poly_str);
         // print_poly(p);
         Polynomial coeff;
         poly_man.poly_partial_div(coeff, poly_assume, p);
+        // print_poly(coeff);
         Polynomial poly_sub;
         for (int i = 0; i < coeff.length(); ++i) {
             Monomial mono = coeff[i];
+            // print_mono(mono);
             std::string mono_str = mono.str();
             for (auto var : vars) {
                 if (mono_str.find(var) != std::string::npos) {
@@ -219,6 +348,11 @@ void RWManager::parsePoly(std::string filepath) {
             }
         }
         coeff = poly_man.poly_sub(coeff, poly_sub);
+
+        Polynomial erase_poly = poly_man.poly_mul(p, coeff);
+        poly_remain = poly_man.poly_sub(poly_remain, erase_poly);
+
+        // print_poly(poly_sub);
         // print_poly(coeff);
         printf("(assert (= %d (mod (", coeff_int);
         if (coeff.length() > 1) {
@@ -242,7 +376,99 @@ void RWManager::parsePoly(std::string filepath) {
             printf(")");
         }
         printf(" %d)))\n", (1 << outbit));
-        std::cout << std::endl;
+    }
+
+    // print_poly(poly_remain);
+
+    std::vector<Polynomial> all_comb;
+    for (auto term : term_set) {
+        std::vector<std::string> term_vec(term.begin(), term.end());
+
+        if (term_vec.size() == 1) {
+            std::string var = term_vec[0];
+            for (int i = 0; i < var_bit[var]; ++i) {
+                std::string var_expand = "(1 1 " + var + "_" + std::to_string(i) + " 1)";
+                Polynomial var_poly = poly_man.parse(var_expand);
+                // print_poly(var_poly);
+                all_comb.push_back(var_poly);
+            }
+        } else {
+            std::vector<Polynomial> partial_comb;
+            std::string var1 = term_vec[0];
+            std::string var2 = term_vec[1];
+            for (int i = 0; i < var_bit[var1]; ++i) {
+                std::string var1_expand = "(1 1 " + var1 + "_" + std::to_string(i) + " 1)";
+                Polynomial var1_poly = poly_man.parse(var1_expand);
+                for (int j = 0; j < var_bit[var2]; ++j) {
+                    std::string var2_expand = "(1 1 " + var2 + "_" + std::to_string(j) + " 1)";
+                    Polynomial var2_poly = poly_man.parse(var2_expand);
+                    Polynomial var_poly = poly_man.poly_mul(var1_poly, var2_poly);
+                    // print_poly(var_poly);
+                    partial_comb.push_back(var_poly);
+                }
+            }
+
+            for (int i = 2; i < term_vec.size(); ++i) {
+                std::vector<Polynomial> partial_comb_updated;
+                std::string var1 = term_vec[i];
+                std::string var1_expand = "(1 1 " + var1 + "_" + std::to_string(i) + " 1)";
+                Polynomial var1_poly = poly_man.parse(var1_expand);
+                for (int j = 0; j < partial_comb.size(); ++j) {
+                    Polynomial var_poly = poly_man.poly_mul(var1_poly, partial_comb[j]);
+                    partial_comb_updated.push_back(var_poly);
+                }
+                partial_comb = partial_comb_updated;
+            }
+            all_comb.insert(all_comb.end(), partial_comb.begin(), partial_comb.end());
+        }
+    }
+    for (int i = 0; i < all_comb.size(); ++i) {
+        // print_poly(all_comb[i]);
+
+        Polynomial q;
+        poly_man.poly_partial_div(q, poly_remain, all_comb[i]);
+        if (q.empty()) continue;
+
+        Polynomial poly_sub;
+        for (int i = 0; i < q.length(); ++i) {
+            Monomial mono = q[i];
+            // print_mono(mono);
+            std::string mono_str = mono.str();
+            for (auto var : vars) {
+                if (mono_str.find(var) != std::string::npos) {
+                    Polynomial p = poly_man.getPoly({mono});
+                    poly_sub = poly_man.poly_add(poly_sub, p);
+                    break;
+                }
+            }
+        }
+        q = poly_man.poly_sub(q, poly_sub);
+        if (q.empty()) continue;
+        // print_poly(q);
+
+        printf("(assert (= %d (mod (", 0);
+        if (q.length() > 1) {
+            printf("+ ");
+            for (int i = 0; i < q.length(); ++i) {
+                Monomial mono = q[i];
+                printf("(* ");
+                std::set<Var> vars = mono.vars();
+                for (auto var : vars) {
+                    printf("%s ", var.name());
+                }
+                printf(") ");
+            }
+            printf(")");
+        } else {
+            printf("* ");
+            std::set<Var> vars = q.vars();
+            for (auto var : vars) {
+                printf("%s ", var.name());
+            }
+            printf(")");
+        }
+        printf(" %d)))\n", (1 << outbit));
+
     }
 }
 
